e684fe9ec53c6bfd340f8a82d0051947db3746d1
James <james@seamlessthemes.com>
2013-02-18 11:01:08 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,36 @@
+			<footer class="footer" role="contentinfo">
+			
+				<div id="inner-footer" class="wrap clearfix">
+
+						<div id="signup" class="twelvecol">
+
+					    	<input class="signup-input" placeholder="First Name">
+
+					    	<input class="signup-input" placeholder="Last Name">
+
+					    	<input class="signup-input" placeholder="Email">
+
+					    	<a class="button" href="">Start your FREE DEMO today!</a>
+
+					    </div>
+					
+					<?php get_sidebar( 'sidebar2' ); ?>
+
+					<nav role="navigation">
+    					<?php bones_footer_links(); ?>
+	                </nav>
+	                		
+					<p class="source-org copyright">&copy;2012 all rights reserved</p>
+				
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+		
+		<!-- all js scripts are loaded in library/bones.php -->
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end page. what a ride! -->
---
+++ page-custom.php
@@ -0,0 +1,118 @@
+<?php
+/*
+Template Name: Custom Page Example
+*/
+?>
+
+<?php get_header(); ?>
+			
+			<div id="content">
+			
+				<div id="inner-content" class="wrap clearfix">
+			
+				    <div id="main" class="twelvecol first clearfix" role="main">
+
+				    	<div id="main-area">
+
+				    		<div id="banner">
+
+				    			<h2>Simply the easiest and most cost-effective</h2>
+				    			<h3>online restaurant marketing service.</h3>
+
+				    			<div id="banner-buttons">
+
+					    			<a href="" class="button">start a FREE trial!</a>
+
+					    			<a href="google.com">watch demo</a>
+
+					    		</div>
+
+				    		</div>
+
+						    <div id="asseenin" class="twelvecol">
+
+						    	<h3>AS SEEN IN:</h3>
+
+						    </div>
+
+						</div>
+
+						<div id="get-found">
+
+							<div class="sixcol front-content-right">
+
+								<img class="icon-left" src="/wp-content/themes/f4f/library/images/getfoundicon.gif" />
+
+								<h2>GET FOUND</h2>
+								<h6>BY USING OUR EFFECTIVE SEO TOOLS</h6>
+
+								<p>Attract new customers by dominating the first pages of Google <br /> for searches related to your restaurant.</p>
+
+								<p>+ read more</p>
+
+								<img src="/wp-content/themes/f4f/library/images/google.jpg" />
+
+							</div>
+
+						</div>
+
+						<div id="get-loyalty">
+
+							<div class="sixcol front-content-left">
+
+								<img class="icon-right" src="/wp-content/themes/f4f/library/images/getloyaltyicon.gif" />
+
+								<h2>GET LOYALTY</h2>
+								<h6>WITH YOUR OWN MOBILE APP</h6>
+
+								<h5>Retain your new customers with your restaurant's own mobile app.</h5>
+
+								<p>Marketing a restaurant is tough, complicated and expensive. With so many places to spend your money like direct mail coupons, reviews sites, newspapers and other traditional advertising, your revenue can get sopped up quick. Not to mention, all of this is geared towards just getting new customers in the door – what about keeping them coming back? Centralize your marketing and reduce spending by using your restaurant's own mobile app where you can offer customers loyalty deals, check-in specials, events, push notifications, and much more to keep them coming back. </p>
+
+							</div>
+
+						</div>
+
+						<div id="get-shared">
+
+							<div class="sixcol front-content-right">
+
+								<img class="icon-left" src="/wp-content/themes/f4f/library/images/getsharedicon.gif" />
+
+								<h2>GET SHARED</h2>
+								<h6>VIA ALL THE SOCIAL MEDIA OUTLETS</h6>
+
+								<h5>Attract new customers by dominating the first pages of Google <br /> for searches related to your restaurant.</h5>
+
+								<p>When your potential customers are looking for a place to eat, very often they will go to Google. Is your restaurant's content showing up for searches related to what you offer? Flip4Food's online restaurant marketing engine is designed to give Google exactly what it likes to get your restaurant top search engine visibility. We do this with our proprietary mix of keyword research, online video distribution, social media, and Flip4Food.tv's powerful website.</p>
+
+							</div>
+
+						</div>
+
+						<div id="get-seen">
+
+							<div class="sixcol front-content-left">
+
+								<img class="icon-right" src="/wp-content/themes/f4f/library/images/getseenicon.gif" />
+
+								<h2>Pricing</h2>
+								<h6>ONE PRICE. SO MANY OPPORTUNITIES.</h6>
+
+								<p>Marketing a restaurant is tough, complicated and expensive. With so many places to spend your money like direct mail coupons, reviews sites, newspapers and other traditional advertising, your revenue can </p>
+
+								<img class="pricing-breakdown-graph" src="/wp-content/themes/f4f/library/images/pricing-breakdown.gif" />
+
+								<p class="clear warranty">* there is a 1 year minimum required. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque ante non est placerat scelerisque.  $300 startup fee.  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque ante non est placerat scelerisque. $200 cancellation fee. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc scelerisque ante non est placerat scelerisque. </p>
+
+							</div>
+
+						</div>
+			
+				    </div> <!-- end #main -->
+				    
+				</div> <!-- end #inner-content -->
+    
+			</div> <!-- end #content -->
+
+<?php get_footer(); ?>
